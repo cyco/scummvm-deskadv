32996ca429c9f4a915d7920a9ac49798f577cb92
Christoph Leimbrock <christoph.leimbrock@gmx.de>
2015-11-09 14:26:13 +0100

    DESKADV: Add info on save game format.

--- saveload.cpp
+++ saveload.cpp
@@ -35,3 +35,77 @@ Saveload::~Saveload() {
 }
 
 } // End of namespace Deskadv
+
+/* save game format (yoda stories)
+ *
+ * [9] file magic ('INDYSAV44' or 'YODASAV44')
+ * [4] unknown
+ * [4] unknown
+ * [4] unknown
+ * [2] puzzle count 1
+ *     [2] puzzle idx
+ * [2] puzzle count 2
+ *     [2] puzzle idx
+ * world(2*2) (dagobah)
+ * world(10*10)
+ * [4] inventory count
+ *     [2] tile idx
+ * [2] current zone id
+ * [4] current zone x (in world coordinates)
+ * [4] current zone y (in world coordinates)
+ * [2] current weapon
+ *     if weapon != 0xFFFF: [2] ammo
+ * [2] force ammo
+ * [2] blaster ammo
+ * [2] blaster rifle ammo
+ * [4] hero x on zone (in px)
+ * [4] hero y on zone (in px)
+ * [4] damage
+ * [4] lives
+ * [4*4] unknown
+ * [4] end puzzle id
+ * [4] end puzzle id again
+ *
+ * world(n) :=
+ * n zone headers
+ * [4] x
+ * [4] y
+ * while x,y != 0xFFFF FFFF
+ *     [2] zone id
+ *     [4] visited
+ *     [4] unknown
+ *     if visited:
+ *          [16] unknown
+ *          [zwidth * zheight * 3] tile id
+ *          [4] unknown
+ *     [4] hotspot count
+ *          [2] arg1
+ *          [2] arg2
+ *          [4] type
+ *          [2] x
+ *          [2] y
+ *     if visited:
+ *          [4] npc count
+ *              [2] char idx
+ *              [2] x
+ *              [2] y
+ *              [86] unknown
+ *          [4] action count
+ *              [2] action state info 1
+ *              [2] action state info 2
+ *      for each 'DoorIn' hotspot with a valid zone id
+ *           goto loop body without updating x,y
+ *
+ * zone header :=
+ * [1]  visited
+ * [19] unknown
+ * [2]  zoen id
+ * [2]  unknown
+ * [2]  item to be used / lost
+ * [2]  item to be gained
+ * [2]  unknown
+ * [2]  another item to be used / lost
+ * [2]  another item to be gained
+ * [8]  unknown
+ *
+ */
